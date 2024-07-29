ad3d4912a81db7f60b6a2d5c7ecd1e1dbfad932f
Cndynajae <csagitasari@gmail.com>
2024-07-29 14:14:51 +0700

    Belajar API

--- app/Http/Controllers/Api/LigaController.php
+++ app/Http/Controllers/Api/LigaController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Liga;
-use Validator;
+use Illuminate\Support\Facades\Validator;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
